0ec407dd1937da9ecb69fda35d0d2df14c83825d
DiegoVelasco84 <83100265+DiegoVelasco84@users.noreply.github.com>
2023-09-19 09:26:15 -0500

    Ajustes 5

--- src/config/global.js
+++ src/config/global.js
@@ -413,6 +413,12 @@ export default {
           centro:
             'Centro para la Industria de la Comunicación Gráfica - Regional Distrito Capital',
         },
+        {
+          nombre: 'Gloria Lida Alzáte Suarez',
+          cargo: 'Adecuación instruccional - 2023',
+          centro:
+            'Centro de Gestión de Mercados, Logística y Tecnologías de la Información - Regional Distrito Capital',
+        },
       ],
     },
     {
@@ -483,8 +489,14 @@ export default {
             'Centro de Gestión de Mercados, Logística y Tecnologías de la Información - Regional Distrito Capital',
         },
         {
-          nombre: 'Leyson Fabian Castaño Pérez',
+          nombre: 'Lina Marcela Pérez Manchego',
           cargo: 'Validación de recursos educativos digitales',
+          centro:
+            'Centro de Gestión de Mercados, Logística y Tecnologías de la Información - Regional Distrito Capital',
+        },
+        {
+          nombre: 'Leyson Fabian Castaño Pérez',
+          cargo: 'Validación y vinculación LMS',
           centro:
             'Centro de Gestión de Mercados, Logística y Tecnologías de la Información - Regional Distrito Capital',
         },
